dcfe565e945910a4c7a68211bc2e1ade11e36d05
jason <jbut@swin.edu.au>
2026-07-21 21:13:39 +1000

    First cut at efibootmgr wrapper sub-module

---
+++ pyefiboot/efibootmgr.py
@@ -0,0 +1,147 @@
+import logging
+import shutil
+import subprocess
+
+
+__log = logging.getLogger(__name__)
+"""Create the module logger"""
+
+__efibootmgr_cmd = shutil.which('efibootmgr')
+"""Locate the efibootmgr command, module raises exception if not found"""
+
+if __efibootmgr_cmd is None:
+    raise RuntimeError(f'The "efibootmgr" executable could not be found on your system')
+
+
+def __execute_efibootmgr(args: list[str]) -> subprocess.CompletedProcess:
+    """
+    Execute the efibootmgr command with the provided parameters
+
+    :param args: List of strings containing the command line arguments. As a private function, no need to validate args is a list of strings
+    :return:
+    :raise subprocess.CalledProcessError: If efibootmgr command fails
+    """
+    __log.debug(f'Executing efibootmgr with parameters: {args}')
+    result = subprocess.run([__efibootmgr_cmd] + args, capture_output=True, text=True, check=True)
+    return result
+
+
+def __validate_bootnum_param(param: int | str) -> str:
+    """
+    Validate that param is either an integer mapping to a valid boot number (0000-ffff) or a string matching a valid boot number
+
+    :param param: Integer or string value containing the parameter to validate
+    :return: Sanitised string representation of boot number that can be passed to efibootmgr
+    :raise ValueError: If param is not an integer or a valid boot number
+    """
+    if isinstance(param, int) and 0 <= param <= 0xffff:
+        return f'{param:04x}'
+
+    if isinstance(param, str):
+        if len(param) == 4 and all(c in '0123456789abcdefABCDEF' for c in param):
+            return param
+
+    raise ValueError(f'Provided parameter({param} must be integer or string containing hexadecimal value in range 0x0000-0xffff')
+
+
+def set_boot_next(entry: int | str):
+    """
+    Set the BootNext variable to entry
+
+    :param entry: Boot entry in range 0x0000-0xffff. Must be valid integer in range or string value matching requirements
+    """
+    entry = __validate_bootnum_param(entry)
+
+    __log.debug(f'Setting BootNext to {entry}')
+    __execute_efibootmgr(['--bootnext', entry])
+
+
+def delete_boot_next():
+    """
+    Delete the currently set BootNext entry
+    """
+    __log.debug('Removing BootNext variable')
+    __execute_efibootmgr(['--delete-bootnext'])
+
+
+def set_boot_order(entries: list[int] | list[str]):
+    """
+    Set the BootOrder variable to the list of boot entry numbers provided in entries
+
+    :param entry: Boot entry in range 0x0000-0xffff. Must be valid integer in range or string value matching requirements
+    """
+    if (not isinstance(entries, list)  # raise exception if entries is not a list
+            or not entries  # raise exception if entries is an empty list
+            or not (all(isinstance(entry, int) for entry in entries)  # Raise exception if not list of all integers or list of all strings
+                    or all(isinstance(entry, str) for entry in entries))):
+        raise ValueError(f'Provided list of entries({entries}) must be a list of integers or a list of strings')
+
+    entries = [__validate_bootnum_param(entry) for entry in entries]
+
+    __log.debug(f'Setting boot order to: {entries}')
+    __execute_efibootmgr(['--bootorder', ','.join(entries)])
+
+
+def delete_boot_order():
+    """
+    Delete the currently set BootNext entry
+    """
+    __log.debug('Removing all entries in the BootOrder variable')
+    __execute_efibootmgr(['--delete-bootorder'])
+
+
+def delete_entry(entry: int | str):
+    """
+    Execute efibootmgr to delete the nominated boot entry number
+
+    :param entry: Boot entry in range 0x0000-0xffff. Must be valid integer in range or string value matching requirements
+    """
+    entry = __validate_bootnum_param(entry)
+
+    __log.debug(f'Deleting entry {entry}')
+    __execute_efibootmgr(['--delete-bootnum', '--bootnum', entry])
+
+
+def add_entry(self, kernel):
+    pass
+    # print_command('Adding EFI Entry')
+    # result = subprocess.run([ '/usr/sbin/efibootmgr', '--create-only', '--disk', self.bootdisk.get_disk(), '--part', self.bootdisk.get_partition(), '--label', kernel.efilabel, '--loader', kernel.efifile, '--unicode', kernel.efiextra ], capture_output=True, text=True)
+    #
+    # if result.returncode != 0: raise Exception(f'Error running efibootmgr\n\n{result.stderr}')
+    # print_result(f'Label({kernel.efilabel}) InitRD({kernel.initrd_file})')
+
+
+####################################################################################################
+## The efibootmgr class allows for reading the current contents of the EFI Boot Table and to make ##
+## updates. It only cares about boot entries for ondisk EFI stub loaders                          ##
+##                                                                                                ##
+## Member methods:                                                                                ##
+##   efibootmgr()                - Retrieve the current contents of the EFI boot table            ##
+##   get_kernellist()            - Return a list of kernel file names as identified in EFI        ##
+##   update(kernel_file, kernel) - Update the EFI table entry for the provided kernel filename    ##
+##                                 (kernel_file) and kernel instance (kernel). kernel is None if  ##
+##                                 the kernel does not exist on disk                              ##
+##   update_bootorder()          - Update EFI boot order to be installed kernel versions in       ##
+##                                 descending order, followed by any pre-existing boot order      ##
+##                                 values that do not refer to an ondisk kernel                   ##
+##   display()                   - Dump the boot table to screen                                  ##
+####################################################################################################
+class efibootmgr:
+    def __init__(self, efidisk):
+        """
+        Initialise efibootmgr class
+
+        Retrieve the current contents of the EFI boot table via _read_efi() and set the unchanged flag to True. This flag is cleared if we actually update an
+        EFI boot table entry so we can determine if/when the boot order also needs to be updated
+        :param efidisk:
+        """
+        self.__log = logging.getLogger(self.__class__.__name__)
+
+        self.__efibootmgr_cmd = shutil.which('efibootmgr')
+        if self.__efibootmgr_cmd is None:
+            raise RuntimeError(f'The "efibootmgr" executable could not be found on your system')
+
+        self.bootdisk = efidisk
+
+        self.efi_unchanged = True
+
