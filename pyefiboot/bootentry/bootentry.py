b2091aae5e272edb0eaa77d8b2b477951f3493f2
jason <jbut@swin.edu.au>
2026-07-24 22:04:51 +1000

    code cleanup

--- pyefiboot/bootentry/bootentry.py
+++ pyefiboot/bootentry/bootentry.py
@@ -12,46 +12,11 @@ class BootEntry(EFIVarBase):
         """
         Read an EFI Variable represented as a single integer from the EFI file and store in __value
 
-        .. warning::
-           ONLY one of global_namespace or efivar_fullpath must be provided
+        **WARNING**: ONLY one of global_namespace or efivar_fullpath must be provided
 
         :param efivar_name: EFI variable name to read
         :param efivar_fullpath: Fully qualified path of the EFI Variable file
         """
-        def decode_optional_data(optional_data: bytes) -> str:
-            try:
-                # Try decoding using UTF-16 first
-                result = optional_data.decode('utf-16le').strip('\x00')
-                if result.isprintable(): return result + ' - ' + ':'.join(f'{b:02x}' for b in optional_data)
-            except UnicodeDecodeError:
-                pass
-
-            try:
-                # Try decoding using UTF-16 first
-                result = optional_data.decode('utf-8').strip('\x00')
-                if result.isprintable(): return result
-            except UnicodeDecodeError:
-                pass
-
-            return ':'.join(f'{b:02x}' for b in optional_data) + ' - ' + ''.join(chr(b) if 32 <= b <= 126 else '.' for b in optional_data)
-
-            try:
-                if len(optional_data) > 1 and 0 in optional_data[:2]:
-                    # If optional_data has at least two bytes AND one of these is a zero byte, then this could be a UTF-16 string, otherwise definitely NOT UTF-16
-                    result = optional_data.decode('utf-16le', errors='ignore')
-                    if all(c.isprintable() or c.isspace() for c in
-                           result): return f'{result}raw<{optional_data}>u8<{optional_data.decode("utf-8", errors='ignore')}>'
-
-                # UTF-16 failed, try UTF-8
-                result = optional_data.decode('utf-8', errors='ignore').strip(' \x00')
-                if all(c.isprintable() or c.isspace() for c in result): return f'{result}raw<{optional_data}>u8<{optional_data.decode("utf-8", errors='ignore')}>'
-
-            except UnicodeDecodeError:
-                pass
-
-            # Error decoding OR both UTF-16 and UTF-8 failed
-            return f'<Binary Data>raw<{optional_data}>u8<{optional_data.decode("utf-8", errors='ignore')}>u16<{optional_data.decode("utf-16le", errors="ignore")}>'
-
         super().__init__(efivar_name, efivar_fullpath)
 
         self.__index: str = self.efivar_name[4:]
@@ -103,10 +68,18 @@ class BootEntry(EFIVarBase):
         """
         return f'Boot{self.__index}{'*' if self.__is_active else ''} {self.__label}'
 
+    def delete(self) -> None:
+        """
+        Delete this Boot Entry UEFI variable
+
+        **NOTE**: Class instance will be invalid after calling this method. Should delete instance
+        """
+        self._log.debug(f'Deleting Boot Entry {self.__index} UEFI variable')
+        self._log.info('Functionality not implemented yet')
+        pass
+
     def verbose_str(self) -> str:
-        """
-        :return: Verbose string representation of the Boot Entry
-        """
+        """:return: Verbose string representation of the Boot Entry"""
         return f'{self} - {self.__path_list} - Extra({self.__optional_data})'
 
     # ---------- PROPERTIES ----------
