179174773a775d2a6167ba20f6bd3b4ee1428904
jason <jbut@swin.edu.au>
2026-08-01 19:36:46 +1000

    kernel_entries now a defaultdict (easier to add multiple boot entries with same kernel name/file)
    New property to give access to boot_entries, will allow caller to directly edit a boot entry (once that functionality is implemented)
    
    delete_by_index now loops through everything and is able to update internal variables and with appropriate excptions. Still not complete until boot entry actually deleted which is not happening yet
    
    New  delete_by_kernel to allow deletion of all entries associated with the one file name. Same issues as above, all internal logic works but no entries actually deleted

--- pyefiboot/bootmanager.py
+++ pyefiboot/bootmanager.py
@@ -1,4 +1,5 @@
 import logging
+from collections import defaultdict
 from pyefiboot import Configuration, BootCurrent, BootNext, BootTimeout, BootOrder, BootEntry
 
 
@@ -10,15 +11,15 @@ class BootManager:
         self.__boot_next: BootNext = BootNext()
         self.__boot_timeout: BootTimeout = BootTimeout()
         self.__boot_order: BootOrder = BootOrder()
-        self.__boot_entries: dict[str, BootEntry] = {}
-        self.__kernel_entries: dict[int, BootEntry] = {}
+        self.__boot_entries: dict[int, BootEntry] = {}
+        self.__kernel_entries: defaultdict[str, list[BootEntry]] = defaultdict(list)
 
         self._read_boot_entries()
 
     def _read_boot_entries(self):
         """(Re-)Create the BootEntry objects for all current Boot Entries and store copies in the __boot_entries and __kernel_entries dictionaries"""
         self.__boot_entries = {}
-        self.__kernel_entries = {}
+        self.__kernel_entries = defaultdict(list)
 
         # For each Boot Entry file in the efifs file system
         for boot_entry_file in sorted(Configuration().efivarfs_path.glob('Boot[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]-*')):
@@ -28,7 +29,7 @@ class BootManager:
             self.__boot_entries[entry.index] = entry
             if entry.kernel_file:
                 # If BootEntry has a kernel file specified, add to __kernel_entries as well
-                self.__kernel_entries[entry.kernel_file] = entry
+                self.__kernel_entries[entry.kernel_file].append(entry)
 
     def refresh(self):
         """Update all EFI variables by re-reading from NVRAM"""
@@ -59,8 +60,39 @@ class BootManager:
         """:return: Return internal BootOrder variable"""
         return self.__boot_order
 
+    @property
+    def boot_entries(self) -> dict[int, BootEntry]:
+        """:return: Return internal variable storing dictionary of BootEntry instances"""
+        return self.__boot_entries
+
+    def _delete_from_kernel_entry(self, kernel_file: str, entry: BootEntry):
+        self.__log.debug(f'Delete Boot Entry Boot{entry.hex_index} from entries associated with "{kernel_file}"')
+        self.__kernel_entries[kernel_file] = [x for x in self.__kernel_entries[kernel_file] if x != entry]
+        if not self.__kernel_entries[kernel_file]: del self.__kernel_entries[kernel_file]
+
     def delete_entries_by_index(self, indexes: list[int]):
-        print(f'Deleting the following boot entries: {indexes}')
+        for index in indexes:
+            try:
+                print(f'Deleting boot entry {index}')
+                # Try to extract the boot entry with index, exception will be raised if it does not exist
+                bootentry = self.__boot_entries[index]
+                print(f'Need to delete boot entry: Boot{bootentry.hex_index}')
+                if bootentry.kernel_file:
+                    self._delete_from_kernel_entry(bootentry.kernel_file, bootentry)
+            except KeyError as e:
+                raise ValueError(f'Deleting Boot Entry Boot{e.args[0]:04X}: Boot Entry does not exist') from None
+
+    def delete_entries_by_kernel_file(self, kernel_files: list[str]) -> None:
+        for kernel_file in kernel_files:
+            try:
+                print(f'Deleting all boot entries associated with {kernel_file}')
+                for bootentry in self.__kernel_entries[kernel_file]:
+                    print(f'Need to delete boot entry: {bootentry.hex_index}')
+                    del self.__boot_entries[bootentry.index]
+                del self.__kernel_entries[kernel_file]
+
+            except KeyError as e:
+                raise ValueError(f'Deleting Boot Entries with kernel {e.args[0]}: No entries associated with this kernel') from None
 
     def display(self, verbose: bool = False):
         """
@@ -76,3 +108,5 @@ class BootManager:
         # Display all available boot entries
         for boot_entry in self.__boot_entries.values():
             print(boot_entry.verbose_str() if verbose else boot_entry)
+
+        print(self.__kernel_entries)
